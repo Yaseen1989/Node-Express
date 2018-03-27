dfc4ffaa37a1c6fb8b0696ccd44198bff5a7d335
Yaseen Alkhafaji <eng.yaseen89@outlook.com>
2018-03-27 17:44:12 +0000

    Adding JSon File

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 const express = require("express");
 const app = express();
+const dataFile = require("./data/data.json");
 
 app.get('/', (req, res) => {
    res.send("Hellow from NodeJS and Express"); 
+});
+app.get('/developer', (req, res) => {
+    var info = '';
+    dataFile.developer.forEach(function(item){
+       info += `
+        <li>
+        <h1>${item.name}</h1>
+        <p>${item.summery}</p>
+        </li>
+       ` 
+    });
+   res.send(`
+    <h1>Developer</h1>
+    ${info}
+   `); 
 });
 
 app.listen(process.env.PORT, process.env.IP, () => {
